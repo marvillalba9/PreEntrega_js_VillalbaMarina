368372ca1eeead5c65ea477c4f9f0496d4dd9416
marvillalba9 <maruja.villalba@gmail.com>
2023-03-17 11:40:37 -0300

    agregar fetch

--- js/main.js
+++ js/main.js
@@ -10,27 +10,40 @@ class Productos {
     }
 }
 
+
+function obtenerProductosDelJSON () {
+    fetch("./js/productos.json")
+        .then((response)=> {
+            return response.json ();
+        })
+        .then((productosJSON) => {
+
+            for (const productoJSON of productosJSON) {
+                productos.push(new Productos(
+                    productoJSON.id,
+                    productoJSON.imagen,
+                    productoJSON.tipoDeProducto,
+                    productoJSON.sabor,
+                    productoJSON.descripcion,
+                    productoJSON.precio,
+                    productoJSON.cantidad,
+                ));
+                }
+
+            cargarProductos(productos);
+        });
+}
+
 const productos = [];
 let carrito = [];
 
-productos.push (new Productos (1, "../imagenes/cuplimon.jpg", "Cupcakes", "Limon", "Cupcake con base de limón combinado con buttercream de vainilla. Opcionalmente se puede combinar con relleno de arandanos o buttercream de limón.", 50, 1));
-productos.push (new Productos (2, "../imagenes/cupchoco.jpg", "Cupcakes", "Chocolate", "Delicioso cupcake de intenso chocolate, decorado con buttercream a base de chocolate.", 50, 1));
-productos.push (new Productos (3, "../imagenes/cupvainilla.jpg", "Cupcakes", "Vainilla", "Nuestro clásico cupcake de vainilla, con una sencilla pero deliciosa buttercream de vainilla.", 40, 1));
-productos.push (new Productos (4, "../imagenes/redvelvet.jpg", "Cupcakes", "Red Velvet", "Exquisito cupcake de red velvet decorado con buttercream a base de queso crema.", 60, 1));
-productos.push (new Productos (5, "../imagenes/brownie.jpg", "Tortas", "Brownie", "Humedo y delicioso brownie con nueces como base, una generosa capa de dulce de leche repostero y merengue italiano para culminar.", 1800, 1));
-productos.push (new Productos (6, "../imagenes/chocotorta.jpg", "Tortas", "Chocotorta", "Unica e irremplazable chocotorta, con las galletitas que amamos y la deliciosa crema chocotorta.", 1800, 1));
-productos.push (new Productos (7, "../imagenes/cheesecakefrutos.jpg", "Tortas", "Cheesecake de Frutos Rojos", "Clásico cheesecake, con base de galletitas y deliciosamente combinado con frutos rojos de estación.", 2300, 1));
-productos.push (new Productos (8, "../imagenes/cheesecakechoco.jpg", "Tortas", "Cheesecake de Chocolate", "Base de galletitas de chocolate, sobre la cual armamos una deliciosa y fresca cheesecake con chocolate amargo.", 2300, 1));
-productos.push (new Productos (9, "../imagenes/lemonpie.jpg", "Tortas", "Lemon Pie", "Fresco y clásico, base de delicada masa sablé, relleno cremoso de limón y merengue italiano para completar.", 2500, 1));
-productos.push (new Productos (10, "../imagenes/moussefrutilla.jpg", "Tortas", "Mousse de Frutilla", "Fresca y riquisima mousse de frutilla, sobre base de galletitas de chocolate.", 2500, 1));
-productos.push (new Productos (11, "../imagenes/rogel.jpg", "Tortas", "Rogel", "El clásico rogel, capas y capas de crocante hojaldre, unidas por el más rico dulce de leche repostero, terminación con merengue italiano.", 2100, 1));
-productos.push (new Productos (12, "../imagenes/manzana.jpg", "Tortas", "Crumble de Manzana", "Base de masa sablé, relleno suave y delicioso de manzana, con leve perfume de especias, coronado con el más crocante crumble.", 2100, 1));
-
-console.log (productos);
+obtenerProductosDelJSON ();
 
 const contenedor = document.querySelector ("#contenedor");
 const vaciarCarrito = document.querySelector("#vaciarCarrito");
 const precioTotal = document.querySelector ("#precioTotal");
+const carritoContenedor = document.querySelector("#carritoContenedor");
+const finalizarCompra = document.querySelector ("#finalizarCompra");
 
 
 
@@ -39,29 +52,67 @@ document.addEventListener ("DOMContentLoaded", ()=> {
     mostrarCarrito ();
 })
 
-productos.forEach((prod) => {
-    const {id, imagen, tipoDeProducto, sabor, descripcion, precio} = prod;
-    contenedor.innerHTML += `
-		<div class="card h-100">
-        <div class="card h-100">
-        <img src="${imagen}" class="card-img-top"
-            alt="cupcake de chocolate con buttercream de chocolate">
-            <div class="card-body">
-                <h5 class="card-title">${tipoDeProducto} de ${sabor}</h5>
-                <p class="card-text text-wrap text-center"> ${descripcion
-                } Precio por unidad $ <b> ${precio}</b></p>
-                <a onclick="agregarProducto(${id})" class="btn btn-primary">Comprar</a>
+function cargarProductos () {
+  
+    productos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.innerHTML += `
+		    <div class="card h-100">
+            <div class="card h-100">
+            <img src="${producto.imagen}" class="card-img-top"
+                alt="cupcake de chocolate con buttercream de chocolate">
+                <div class="card-body">
+                    <h5 class="card-title">${producto.tipoDeProducto} de ${producto.sabor}</h5>
+                    <p class="card-text text-wrap text-center"> ${producto.descripcion
+                    } Precio por unidad $ <b> ${producto.precio}</b></p>
+                    <a onclick="agregarProducto(${producto.id})" class="btn btn-primary">Comprar</a>
+                </div>
             </div>
-        </div>
-    </div>
-    `
+            </div>
+            `
+    
+        contenedor.append(div);
 })
+}
+
+//cargarProductos ();
 
 
 vaciarCarrito.addEventListener ("click", () => {
     carrito.length = [];
     mostrarCarrito ();
 })
+
+finalizarCompra.addEventListener ("click", () => {
+          if (carrito.length === 0) {
+            Swal.fire({
+            title: "¡Tu carrito está vacio!",
+            text: "Agrega los productos que mas te tienten!",
+            icon: "warning",
+            confirmButtonText: "Aceptar"
+        }
+        )} else {
+            Swal.fire({
+                title: 'Cerramos el pedido?',
+                text: "Asegurate de no olvidarte nada!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si, ya tengo todo!'
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  Swal.fire(
+                    'Perfecto!',
+                    'Te vamos a redirigir al pago para que terminemos la compra. Gracias!',
+                    'success'
+                  )
+                }
+              })
+        }
+})
+
 
 const agregarProducto = (id) => {
     const existe = carrito.some (prod => prod.id === id)
@@ -97,6 +148,7 @@ const mostrarCarrito = () => {
                         <p>Sabor: ${sabor}</p>
                         <p>Cantidad: ${cantidad}</p>
                         <p>Precio por unidad: $ ${precio}</p>
+                        <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Eliminar producto</button>
                     </div>
                 </div>
             `;
@@ -109,7 +161,8 @@ const mostrarCarrito = () => {
         `;
       } else {
         console.log("Algo");
-      }
+    }
+    carritoContenedor.textContent = carrito.length;
     
       if (precioTotal) {
         precioTotal.innerText = carrito.reduce(
@@ -125,6 +178,14 @@ const mostrarCarrito = () => {
 function guardarStorage() {
     localStorage.setItem("carrito", JSON.stringify(carrito));
 }
+
+function eliminarProducto(id) {
+    const itemId = id;
+    carrito = carrito.filter((productos) => productos.id !== itemId);
+    mostrarCarrito();
+  }
+
+
 
 
 
