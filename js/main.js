3d76b9a192aaf5357808654ff6f0c30d254b132f
marvillalba9 <maruja.villalba@gmail.com>
2023-03-17 15:17:17 -0300

    agrego fetch

--- js/main.js
+++ js/main.js
@@ -1,43 +1,10 @@
-class Productos {
-    constructor (id, imagen, tipoDeProducto, sabor, descripcion, precio, cantidad) {
-        this.id = id;
-        this.imagen = imagen;
-        this.tipoDeProducto = tipoDeProducto;
-        this.sabor = sabor;
-        this.descripcion = descripcion;
-        this.precio = precio;
-        this.cantidad = cantidad;
-    }
-}
 
 
-function obtenerProductosDelJSON () {
-    fetch("./js/productos.json")
-        .then((response)=> {
-            return response.json ();
-        })
-        .then((productosJSON) => {
+const url = "/productos.json";
 
-            for (const productoJSON of productosJSON) {
-                productos.push(new Productos(
-                    productoJSON.id,
-                    productoJSON.imagen,
-                    productoJSON.tipoDeProducto,
-                    productoJSON.sabor,
-                    productoJSON.descripcion,
-                    productoJSON.precio,
-                    productoJSON.cantidad,
-                ));
-                }
-
-            cargarProductos(productos);
-        });
-}
-
-const productos = [];
-let carrito = [];
-
-obtenerProductosDelJSON ();
+fetch (url)
+    .then (res => res.json())
+    .then (data => cargarProductos(data))
 
 const contenedor = document.querySelector ("#contenedor");
 const vaciarCarrito = document.querySelector("#vaciarCarrito");
@@ -47,36 +14,44 @@ const finalizarCompra = document.querySelector ("#finalizarCompra");
 
 
 
+function cargarProductos (productos) {
+    console.log (productos);
+
+    productos.forEach(prod => {
+        let card = document.createElement ("div");
+
+        card.innerHTML = `
+            <div class="card h-100">
+            <div class="card h-100">
+            <img src="${prod.imagen}" class="card-img-top"
+                alt="cupcake de chocolate con buttercream de chocolate">
+                <div class="card-body">
+                    <h5 class="card-title">${prod.tipoDeProducto} de ${prod.sabor}</h5>
+                    <p class="card-text text-wrap text-center"> ${prod.descripcion
+                    } Precio por unidad $ <b> ${prod.precio}</b></p>
+                    <button="agregarProducto(${prod.id})" class="btn btn-primary">Comprar</a>
+                </div>
+            </div>
+        </div>
+        `
+        contenedor.appendChild(card);
+    });
+
+    // const botonComprar = document.querySelectorAll (".btn-primary");
+    // botonComprar.forEach(btn => {
+    //     btn.addEventListener ("click", (e) => agregarProducto(e, productos));
+    // })
+}
+
+const productos = [];
+let carrito = [];
+
+
+
 document.addEventListener ("DOMContentLoaded", ()=> {
     carrito = JSON.parse (localStorage.getItem("carrito")) || [];
     mostrarCarrito ();
 })
-
-function cargarProductos () {
-  
-    productos.forEach(producto => {
-
-        const div = document.createElement("div");
-        div.innerHTML += `
-		    <div class="card h-100">
-            <div class="card h-100">
-            <img src="${producto.imagen}" class="card-img-top"
-                alt="cupcake de chocolate con buttercream de chocolate">
-                <div class="card-body">
-                    <h5 class="card-title">${producto.tipoDeProducto} de ${producto.sabor}</h5>
-                    <p class="card-text text-wrap text-center"> ${producto.descripcion
-                    } Precio por unidad $ <b> ${producto.precio}</b></p>
-                    <a onclick="agregarProducto(${producto.id})" class="btn btn-primary">Comprar</a>
-                </div>
-            </div>
-            </div>
-            `
-    
-        contenedor.append(div);
-})
-}
-
-//cargarProductos ();
 
 
 vaciarCarrito.addEventListener ("click", () => {
@@ -114,6 +89,7 @@ finalizarCompra.addEventListener ("click", () => {
 })
 
 
+
 const agregarProducto = (id) => {
     const existe = carrito.some (prod => prod.id === id)
 
@@ -140,15 +116,15 @@ const mostrarCarrito = () => {
     if (modalBody) {
         modalBody.innerHTML = "";
         carrito.forEach((prod) => {
-            const {id, tipoDeProducto, sabor, precio, cantidad} = prod;
+            
             modalBody.innerHTML += `
                 <div class="modal-contenedor">
                     <div> 
-                        <p>Tipo de Producto: ${tipoDeProducto}</p>
-                        <p>Sabor: ${sabor}</p>
-                        <p>Cantidad: ${cantidad}</p>
-                        <p>Precio por unidad: $ ${precio}</p>
-                        <button class="btn btn-danger"  onclick="eliminarProducto(${id})">Eliminar producto</button>
+                        <p>Tipo de Producto: ${prod.tipoDeProducto}</p>
+                        <p>Sabor: ${prod.sabor}</p>
+                        <p>Cantidad: ${prod.cantidad}</p>
+                        <p>Precio por unidad: $ ${prod.precio}</p>
+                        <button class="btn btn-danger"  onclick="eliminarProducto(${prod.id})">Eliminar producto</button>
                     </div>
                 </div>
             `;
@@ -183,7 +159,7 @@ function eliminarProducto(id) {
     const itemId = id;
     carrito = carrito.filter((productos) => productos.id !== itemId);
     mostrarCarrito();
-  }
+}
 
 
 
